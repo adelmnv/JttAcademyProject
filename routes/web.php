2b94503146c3aa06caeec259f46122b4ef1b4203
adelmnv <99648706+adelmnv@users.noreply.github.com>
2024-05-31 22:55:48 +0600

    маршруты для создания

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::get('/about',[HomeController::class,'about'])->name('about');
 Route::get('/login' ,[HomeController::class,'user_login'])->name('user.login');
 Route::post('/login' ,[HomeController::class,'user_auth'])->name('user.auth');
 Route::get('/logout' ,[HomeController::class,'user_logout'])->name('user.logout');
-
 Route::get('/dash',[HomeController::class,'admin_dash'])->name('user.dash')->middleware('auth');
 
 Route::get('/posts',[PostController::class,'posts'])->name('posts');
@@ -32,15 +31,21 @@ Route::get('/posts/{post_id?}/view/',[PostController::class,'view'])->name('post
 Route::get('/posts/category/by_category/{category_id?}',[PostController::class,'posts_by_category'])->name('posts.by_category');
 Route::get('/posts/{post_id?}/edit/',[PostController::class,'edit'])->name('posts.edit')->middleware('auth');
 Route::post('/posts/{post_id?}/edit/',[PostController::class,'update'])->name('posts.update')->middleware('auth');
+Route::get('/posts/create/',[PostController::class,'create'])->name('posts.create')->middleware('auth');
+Route::post('/posts/create/',[PostController::class,'save'])->name('posts.save')->middleware('auth');
 
 Route::get('/coaches',[CoachController::class,'coaches'])->name('coaches');
 Route::get('/coaches/{coach_id?}/view/',[CoachController::class,'view'])->name('coaches.view')->where('coach_id','[0-9]+');
 Route::get('/coaches/{coach_id?}/edit/',[CoachController::class,'edit'])->name('coaches.edit')->middleware('auth');
 Route::post('/coaches/{coach_id?}/edit/',[CoachController::class,'update'])->name('coaches.update')->middleware('auth');
+Route::get('/coaches/create/',[CoachController::class,'create'])->name('coaches.create')->middleware('auth');
+Route::post('/coaches/create/',[CoachController::class,'save'])->name('coaches.save')->middleware('auth');
 
 Route::get('/players',[PlayerController::class,'players'])->name('players');
 Route::get('/players/{player_id?}/edit/',[PlayerController::class,'edit'])->name('players.edit')->middleware('auth');
 Route::post('/players/{player_id?}/edit/',[PlayerController::class,'update'])->name('players.update')->middleware('auth');
+Route::get('/players/create/',[PlayerController::class,'create'])->name('players.create')->middleware('auth');
+Route::post('/players/create/',[PlayerController::class,'save'])->name('players.save')->middleware('auth');
 
 Route::get('/practices', [PracticeController::class,'practices'])->name('practices');
 Route::get('/practices/type/by_type/{type_id?}',[PracticeController::class,'practices_by_type'])->name('practices.by_type');
@@ -48,6 +53,8 @@ Route::get('/practices/{practice_id?}/type/{type_id?}/create_application/',[Prac
 Route::post('/practices/{practice_id?}/type/{type_id?}/store_application/',[PracticeController::class,'store_application'])->name('practices.store_application');
 Route::get('/practices/{practice_id?}/edit/',[PracticeController::class,'edit'])->name('practices.edit')->middleware('auth');;
 Route::post('/practices/{practice_id?}/update/',[PracticeController::class,'update'])->name('practices.update')->middleware('auth');
+Route::get('/practices/create/',[PracticeController::class,'create'])->name('practices.create')->middleware('auth');;
+Route::post('/practices/create/',[PracticeController::class,'save'])->name('practices.save')->middleware('auth');
 
 
 Route::get('/tournaments',[TournamentController::class,'tournaments'])->name('tournaments');
@@ -56,6 +63,8 @@ Route::get('/tournaments/{tournament_id?}/create_registration/',[TournamentContr
 Route::post('/tournaments/{tournament_id?}/store_registration/',[TournamentController::class,'store_registration'])->name('tournaments.store_registration');
 Route::get('/tournaments/{tournament_id?}/edit/',[TournamentController::class,'edit'])->name('tournaments.edit')->middleware('auth');;
 Route::post('/tournaments/{tournament_id?}/update/',[TournamentController::class,'update'])->name('tournaments.update')->middleware('auth');
+Route::get('/tournaments/create/',[TournamentController::class,'create'])->name('tournaments.create')->middleware('auth');;
+Route::post('/tournaments/create/',[TournamentController::class,'save'])->name('tournaments.save')->middleware('auth');
 
 
 
